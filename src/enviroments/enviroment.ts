6529ed98f34ce6f57ac2b92243a12b1a62f507b8
Lara Souza <laragabriellysouzabatista@gmail.com>
2024-10-24 13:05:26 -0300

    created .env arq

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  bearerToken: 'API_KEY',
+  urlApiBasePath: 'API_URL',
+  mockApiData: false,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import * as dotenv from 'dotenv';
+import { environment } from './enviroments/enviroment';
+import { enableProdMode } from '@angular/core';
 
+dotenv.config();
+
+if (environment.production) {
+  enableProdMode();
+}
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
