23565e19532e0a79948994d6d5cf3984ad6d4811
Lara Souza <laragabriellysouzabatista@gmail.com>
2024-10-24 12:45:09 -0300

    env

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  bearerToken: process.env['API_KEY'],
-  urlApiBasePath: process.env['url_Api_Base_Path'],
+  bearerToken: '',
+  urlApiBasePath: '',
   mockApiData: true,
 };
--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   bearerToken: process.env['API_KEY'],
   urlApiBasePath: process.env['url_Api_Base_Path'],
-  mockApiData: true,
+  mockApiData: false,
 };
